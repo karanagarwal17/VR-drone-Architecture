767502259ed6dd3e1912bbcdc60c634aefeeb940
karanagarwal17 <karanagarwal1702@gmail.com>
2017-03-20 22:21:04 +0530

    completed the compiler

--- compiler.c
+++ compiler.c
@@ -2,91 +2,97 @@
 #include"compiler.h"
 #include<math.h>
 #include<string.h>
+
 int binarytodecimal(char* str){
-  int n = 0;
-  int size = strlen(str) - 1;
-  int count = 0;
-  while ( *str != '\0' ) {
-   if ( *str == '1' )
-       n = n + pow(2, size - count );
-   count++;
-   str++;
-  }
-  return n;
+	int n = 0;
+	int size = strlen(str) - 1;
+	int count = 0;
+	while ( *str != '\0' ) {
+		if ( *str == '1' )
+			n = n + pow(2, size - count );
+		count++;
+		str++;
+	}
+	return n;
 }
 
 void decode(char *a){
-  char b1[4],b2[6],b3[6];
-  int i,x1,x2,x3;
-  for(i=0;i<4;i++){
-    b1[i]=a[i];
-  }
-  for(i=0;i<6;i++){
-    b2[i]=a[i+4];
-  }
-  for(i=0;i<6;i++){
-    b3[i]=a[i+10];
-  }
-  x1 = binarytodecimal(b1);
-  x2 = binarytodecimal(b2);
-  x3 = binarytodecimal(b3);
-  if(x1 == 15){
-    if(x2 == 63){
-      switch(x3){
-        case 0: TAKE();break;
-        case 1: LAND();break;
-        case 2: ILU();break;
-        case 3: SPK();break;
-        case 4: BINF();break;
-      }
-    }
-    else{
-      switch(x2){
-        case 0: PRO(x3,1);break;
-        case 1: PRO(x3,2);break;
-        case 2: PRO(x3,3);break;
-        case 3: SPD(x3,1);break;
-        case 4: SPD(x3,2);break;
-        case 5: SPD(x3,3);break;
-        case 6: GES(x3,1);break;
-        case 7: GES(x3,2);break;
-        case 8: GES(x3,3);break;
-        case 9: REST(x3,1);break;
-        case 10: REST(x3,2);break;
-        case 11: REST(x3,3);break;
-      }
-    }
-  }
-  else{
-    switch(x1){
-      case 0: MOV(x2,x3,4);break;
-      case 1: MOV(x2,x3,5);break;
-      case 2: MOV(x2,x3,6);break;
-      case 3: OBS(x2,x3,4);break;
-      case 4: OBS(x2,x3,5);break;
-      case 5: OBS(x2,x3,6);break;
-      case 6: FP(x2,x3,4);break;
-      case 7: FP(x2,x3,5);break;
-      case 8: FP(x2,x3,6);break;
-      case 9: MVR(x2,x3,7);break;
-      case 10: STR(x2,x3,4);break;
-      case 11: STR(x2,x3,7);break;
-      case 12: LDB(x2,x3,6);break;
-      case 13: LDB(x2,x3,7);break;
-    }
-  }
+	char b1[4],b2[6],b3[6];
+	int i,x1,x2,x3;
+	for(i=0; i<4; i++) {
+		b1[i]=a[i];
+	}
+	for(i=0; i<6; i++) {
+		b2[i]=a[i+4];
+	}
+	for(i=0; i<6; i++) {
+		b3[i]=a[i+10];
+	}
+	x1 = binarytodecimal(b1);
+	x2 = binarytodecimal(b2);
+	x3 = binarytodecimal(b3);
+	if(x1 == 15) {
+		if(x2 == 63) {
+			switch(x3) {
+			case 0: TAKE(); break;
+			case 1: LAND(); break;
+			case 2: ILU(); break;
+			case 3: SPK(); break;
+			case 4: BINF(); break;
+			}
+		}
+		else{
+			switch(x2) {
+			case 0: PRO(x3,1); break;
+			case 1: PRO(x3,2); break;
+			case 2: PRO(x3,3); break;
+			case 3: SPD(x3,1); break;
+			case 4: SPD(x3,2); break;
+			case 5: SPD(x3,3); break;
+			case 6: GES(x3,1); break;
+			case 7: GES(x3,2); break;
+			case 8: GES(x3,3); break;
+			case 9: REST(x3,1); break;
+			case 10: REST(x3,2); break;
+			case 11: REST(x3,3); break;
+			}
+		}
+	}
+	else{
+		switch(x1) {
+		case 0: MOV(x2,x3,4); break;
+		case 1: MOV(x2,x3,5); break;
+		case 2: MOV(x2,x3,6); break;
+		case 3: OBS(x2,x3,4); break;
+		case 4: OBS(x2,x3,5); break;
+		case 5: OBS(x2,x3,6); break;
+		case 6: FP(x2,x3,4); break;
+		case 7: FP(x2,x3,5); break;
+		case 8: FP(x2,x3,6); break;
+		case 9: MVR(x2,x3,7); break;
+		case 10: STR(x2,x3,4); break;
+		case 11: STR(x2,x3,7); break;
+		case 12: LDB(x2,x3,6); break;
+		case 13: LDB(x2,x3,7); break;
+		}
+	}
 }
 
 void main(){
-  int n;
-  FILE *file;
-  char str[30];
-  file = fopen("binary.txt","r");
-  //reading the instructions file and getting the instructions one by one
-  while(fgets(str,30,file)!=NULL){
-    n = strlen(str);
-    str[n-1]='\0';
-    decode(str);
-  }
-  fclose(file);
+	int n;
+	FILE *file;
+	char str[30];
+	file = fopen("binary.txt","r");
+	//reading the instructions file and getting the instructions one by one
+	while(fgets(str,30,file)!=NULL) {
+		n = strlen(str);
+		str[n-1]='\0';
+    printf("%s\n",str);
+    PC++;
+    MAR++;
+    strcpy(MDR,str);
+    strcpy(IR,str);
+		decode(str);
+	}
+	fclose(file);
 }
--- compiler.h
+++ compiler.h
@@ -1,5 +1,6 @@
 #include<string.h>
 #include<unistd.h>
+#include<math.h>
 
 int obstacle[5][2]={0};
 int current_x = 0;
@@ -7,257 +8,285 @@ int current_y = 0;
 int battery = 100;
 int light = 0;
 int PC=0;
+int MAR=0;
+char MDR[17] = {0};
+char IR[17] = {0};
 int speaker = 0;
 int memory[1024] = {0};
 int speed = 1;
 int registers[16] = {0};
 
-void TAKE(){
-  if (battery==100){
-     printf("Drone is taking off\n");
-     battery--;
-     output();}
+void output();
 
-  else{
-      printf("need 100%% battery for take off\n");
-     output();
+void TAKE(){
+	if (battery==100) {
+		printf("Drone is taking off\n");
+		battery--;
+		output();
+	}
+	else{
+		printf("need 100%% battery for take off\n");
+		output();
+	}
 }
 
 void LAND(){
-  strcpy("The drone has landed.",output);
-  current_x = 0;
-  current_y = 0;
-  speaker = 0;
-  light = 0;
-  battery -= 1;
+	printf("The drone has landed\n");
+	current_x = 0;
+	current_y = 0;
+	speaker = 0;
+	light = 0;
+	battery -= 1;
+  output();
 }
 
 void ILU(){
-  if(light == 1){
-    strcpy("The light has been turned off",output);
-    light = 0;
-  }
-  else{
-    strcpy("The drone has been illuminated",output);
-    light = 1;
-  }
-  battery -= 1;
+	if(light == 1) {
+		printf("The light has been turned off\n");
+		light = 0;
+	}
+	else{
+		printf("The drone has been illuminated\n");
+		light = 1;
+	}
+	battery -= 1;
+  output();
 }
 
 void SPK(){
-  if(speaker == 1){
-    strcpy("The speaker has been turned off",output);
-    speaker = 0;
-  }
-  else{
-    strcpy("The speaker has been turned on",output);
-    speaker = 1;
-  }
-  battery -= 1;
+	if(speaker == 1) {
+		printf("The speaker has been turned off\n");
+		speaker = 0;
+	}
+	else{
+		printf("The speaker has been turned on\n");
+		speaker = 1;
+	}
+	battery -= 1;
+  output();
 }
 
 void MVR(int x2, int x3, int s){
-  registers[x3] = x2;
-  strcpy("The value x2 has been stored in registers x3",output);
+	registers[x3] = x2;
+	printf("The value %d has been stored in registers %d\n",x2,x3);
+  battery -= 1;
+  output();
 }
 
 void LDB(int x2, int x3, int s){
-  if(s == 6){
-    registers[x3] = memory[registers[x2]];
-    strcpy("The value at memory location at registers x2 has been stored in registers x3",output);
-  }
-  if(s == 7){
-    registers[x3] = memory[x2];
-    strcpy("The value at memory location at registers x2 has been stored in registers x3",output);
-  }
+	if(s == 6) {
+		registers[x3] = memory[registers[x2]];
+		printf("The value at memory location at registers %d has been stored in registers %d\n",x2,x3);
+	}
+	if(s == 7) {
+		registers[x3] = memory[x2];
+		printf("The value at memory location at registers %d has been stored in registers %d\n",x2,x3);
+	}
+  output();
 }
 
 void STR(int x2, int x3, int s){
-  if(s == 4){
-    memory[x3] = x2;
-    strcpy("The value x2 has been stored at memory location x3",output);
-  }
-  if(s == 7){
-    memory[registers[x3]] = x2;
-    strcpy("The value x2 has been stored at address given by registers x3",output);
-  }
+	if(s == 4) {
+		memory[x3] = x2;
+		printf("The value %d has been stored at memory location %d\n",x2,x3);
+	}
+	if(s == 7) {
+		memory[registers[x3]] = x2;
+		printf("The value %d has been stored at address given by registers %d\n",x2,x3);
+	}
+  output();
 }
 
 void REST(int t,int s){
-  if(s == 3){
-    t = registers[t];
-  }
-  if(s == 2){
-    t = memory[t];
-  }
-  strcpy("Now, the drone will rest",output);
-  sleep(t);
+	if(s == 3) {
+		t = registers[t];
+	}
+	if(s == 2) {
+		t = memory[registers[t]];
+	}
+	printf("Now, the drone will rest\n");
+	sleep(t);
+  output();
 }
 
 void SPD(int x, int s){
-  if(s == 3){
-    x = registers[x];
-  }
-  if(s == 2){
-    x = memory[x];
-  }
-  if(speed > x){
-    strcpy("The speed of the drone has beeen increamented",output);
-  }
-  else if(speed < x){
-    strcpy("The speed of the drone has been decremented",output);
-  }
-  else{
-    strcpy("There is no change in the speed of the drone",output);
-  }
-  speed = x;
-  battery -= 1;
+	if(s == 3) {
+		x = registers[x];
+	}
+	if(s == 2) {
+		x = memory[registers[x]];
+	}
+	if(speed < x) {
+		printf("The speed of the drone has beeen increamented to %d\n",x);
+	}
+	else if(speed > x) {
+		printf("The speed of the drone has been decremented to %d\n",x);
+	}
+	else{
+		printf("There is no change in the speed of the drone. It is %d\n",x);
+	}
+	speed = x;
+	battery -= 1;
+  output();
 }
 
 void BINF(){
-  strcpy("The battery remaining in the drone is - ",output);
-  battery -= 1;
+	printf("The battery remaining in the drone is %d\n",battery);
+	battery -= 1;
+  output();
 }
 
 void PRO(int x, int s){
-  char c[20];
-  int b;
-  FILE * fp;
-  fp = fopen("projects.txt","r");
-  if(s==3)
-  x=registers[x];
-  if(s==2)
-  x=memory[x];
-  while(1)
-  {
-    fscanf(fp,"%[^-]",c);
-    fseek(fp,sizeof(char),SEEK_CUR);
-    fscanf(fp,"%d\n",&b);
-    if(b==x)
-    {
-      printf("PROJECTING THE %s\n",c);
+	char c[20];
+	int b;
+	FILE *fp;
+	fp = fopen("projects.txt","r");
+	if(s==3)
+		x=registers[x];
+	if(s==2)
+		x=memory[registers[x]];
+	while(1)
+	{
+		fscanf(fp,"%[^-]",c);
+		fseek(fp,sizeof(char),SEEK_CUR);
+		fscanf(fp,"%d\n",&b);
+    printf("b = %d, x = %d\n",b,x);
+    if(x == 0 || x > 10){
       break;
     }
-  }
-  battery-=1;
-  output();
+		if(b==x)
+		{
+			printf("PROJECTING THE %s\n",c);
+			break;
+		}
+	}
+	battery-=1;
+	output();
 }
 
 void GES(int x, int s){
-  char c[20];
-  int b;
-  FILE * fp;
-  fp = fopen("projects.txt","r");
-  if(s==3)
-  x=registers[x];
-  if(s==2)
-  x=memory[x];
-  while(1)
-  {
-    fscanf(fp,"%[^-]",c);
-    fseek(fp,sizeof(char),SEEK_CUR);
-    fscanf(fp,"%d\n",&b);
-    if(b==x)
-    {
-      printf("The gesture corresponding to id number %d is: %s\n",b,c);
-      break;
-    }
-  }
-  battery-=1;
-  output();
+	char c[20];
+	int b;
+	FILE * fp;
+	fp = fopen("projects.txt","r");
+	if(s==3)
+		x=registers[x];
+	if(s==2)
+		x=memory[registers[x]];
+	while(1)
+	{
+		fscanf(fp,"%[^-]",c);
+		fseek(fp,sizeof(char),SEEK_CUR);
+		fscanf(fp,"%d\n",&b);
+		if(b==x)
+		{
+			printf("The gesture corresponding to id number %d is: %s\n",b,c);
+			break;
+		}
+	}
+	battery-=1;
+	output();
 }
 
-void MOV(int x, int x, int s){
-  int i,j=0;
-   switch(s){
-     case 4: break;
-     case 5:  x=memory[x];
-              y=memory[y];
-              break;
-    case 6:    y=registers[y];
-               x=registers[x];
-               break;
-   }
-        float y1=y-current_y;
-        float x1=x-current_x;
-        float m=y1/x1;
+void MOV(int x, int y, int s){
+	int i,j=0;
+	switch(s) {
+  	case 4: break;
+  	case 5: {
+      x=memory[x];
+  		y=memory[y];
+    }break;
+  	case 6: {
+      y=registers[y];
+  		x=registers[x];
+    }break;
+	}
+	int y1 = y - current_y;
+	int x1 = x - current_x;
+	float m = (float) y1 / x1;
 
-        int d1=(x-current_x)*(x-current_x);
-        int d2=(y-current_y)*(y-current_y);
-        d1=d1+d2;
-        float d=sqrt(d1);
+	int d1 = x1*x1;
+	int d2 = y1*y1;
+	d1 = d1+d2;
+	float d = sqrt(d1);
 
-        for(i=0;i<5;i++){
-        float k0=(m*(obstacle[i][0]-current_x));
-        int k2=(int)k0;
-         k2=k2+current_y;
-        if(obstacle[i][1]==k2){
-
-           printf("Obstacle (%d,%d) in the path\n",obstacle[i][0],obstacle[i][1]);
-           printf("Taking alternate path\n");
-            j=1;
-           }}
-
-          if(j!=1){
-            printf("No obstacle in the path\n");
-            printf("Taking the path having equation:y=%d(x-%d)+%d\n",(int)m,current_x,current_y);
-            printf("Distance covered=%.2f\n",d);}
-           printf("Drone has moved from (%d,%d) to (%d,%d)\n",current_x,current_y,x,y);
-           current_x=x;
-           current_y=y;
-           battery--;
-           output();
+	for(i=0; i<5; i++) {
+    if(obstacle[i][0] != 0 || obstacle[i][1] != 0){
+  		float k0 = ( m * (obstacle[i][0]-current_x));
+  		int k2= (int)k0;
+  		k2=k2+current_y;
+  		if(obstacle[i][1]==k2) {
+  			printf("Obstacle (%d,%d) in the path\n",obstacle[i][0],obstacle[i][1]);
+  			printf("Taking alternate path\n");
+  			j=1;
+        break;
+  		}
+    }
+	}
+	if(j!=1) {
+		printf("No obstacle in the path\n");
+		printf("Taking the path having equation:y=%d(x-%d)+%d\n",(int)m,current_x,current_y);
+		printf("Distance covered=%.2f\n",d);
+	}
+	printf("Drone has moved from (%d,%d) to (%d,%d)\n",current_x,current_y,x,y);
+	current_x = x;
+	current_y = y;
+	battery--;
+	output();
 }
 
 void OBS(int x, int y, int s){
-  int i,j;
-       switch(s){
-         case 4:  break;
-
-        case 5:   x=memory[x];
-                  y=memory[y];
-                  break;
-
-
-       case 6:    y=registers[y];
-                  x=registers[x];
-                  break;
-     }
-             for(i=0;i<5;i++){
-                 if(obstacle[i][0]==0 &&obstacle[i][1]==0){
-                    obstacle[i][0]=x;
-                    obstacle[i][1]=y;
-                    printf("The obstacle has been set at (%d,%d)\n",x,y);
-                    battery--;
-                    output();
-                    break;}}
-
+	int i,j;
+	switch(s) {
+	case 4:break;
+	case 5:{
+    x=memory[x];
+		y=memory[y];
+  }break;
+	case 6:{
+    y=registers[y];
+		x=registers[x];
+  }break;
+	}
+	for(i=0; i<5; i++){
+		if(obstacle[i][0]==0 && obstacle[i][1]==0) {
+			obstacle[i][0]=x;
+			obstacle[i][1]=y;
+			printf("The obstacle has been set at (%d,%d)\n",x,y);
+			battery--;
+			output();
+			break;
+		}
+	}
 }
 
-void FP(int x, int x, int s){
-  switch(s){
-    case 4:  break;
-
-   case 5:   x=memory[x];
-             y=memory[y];
-             break;
-
-
-  case 6:    y=registers[y];
-             x=registers[x];
-             break;
-  }
-  battery--;
-  output;
-  printf("Drone will follow the player from the location (%d,%d) to the location (%d,%d)\n",current_x,current_y,x,y);
-
+void FP(int x, int y, int s){
+	switch(s){
+  	case 4: break;
+  	case 5:{
+      x=memory[x];
+  		y=memory[y];
+    }break;
+  	case 6:{
+      y=registers[y];
+  		x=registers[x];
+    }break;
+	}
+	battery -=1;
+	printf("Drone will follow the player from the location (%d,%d) to the location (%d,%d)\n",current_x,current_y,x,y);
+  current_x = x;
+  current_y = y;
+	output();
 }
+
 void output(){
-  printf("Battery:%d %%\n",battery);
-  printf("Speaker status:%d\n",speaker);
-  printf("Torch status:%d\n",light);
-  PC++;
-  printf("Program Counter:%d\n",PC);
-  for(int i=0;i<16;i++)
-    printf("R%d=%d\n",i,reg[i]);
-   }
+	printf("Battery: %d %%\n",battery);
+	printf("Speaker status: %d\n",speaker);
+	printf("Torch status: %d\n",light);
+	printf("Program Counter: %d\n",PC);
+  printf("MAR: %d\n",MAR);
+  printf("MDR: %s\n",MDR);
+  printf("IR: %s\n",IR);
+	for(int i=0; i<16; i++)
+		printf("R%d = %d\n",i,registers[i]);
+}
